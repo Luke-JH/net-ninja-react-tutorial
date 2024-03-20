346daca663e0db6e87bbcda3a189e4b4c77bc96c
Luke <ljhorner2@gmail.com>
2024-03-20 17:48:54 +0000

    lesson 4 changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
 function App() {
+  
+  const title = 'welcome to the new blog'
+  const likes = 50
+  const person = {  name: 'yoshi', age: 30}
+  const link = 'https://www.google.com'
+
   return (
     <div className="App">
       <div className='content'>
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{person.name}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
 }
 
-export default App;
+export default App
