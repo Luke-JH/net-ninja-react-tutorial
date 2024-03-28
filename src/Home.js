270ac9963e9e64bad1b4e2ade9194a25bcfb3dfe
Luke <ljhorner2@gmail.com>
2024-03-28 14:34:08 +0000

    L18

--- src/Home.js
+++ src/Home.js
@@ -3,23 +3,29 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [loading, setLoading] = useState(false)
 
 
 
   useEffect(() => {
     // runs every time the data changes / renders
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        console.log(data)
-        setBlogs(data)
-      })
-  }, []) // if empty, the array makes it only run on mounted
+    setLoading(true)
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json()
+        })
+        .then((data) => {
+          console.log(data)
+          setBlogs(data)
+          setLoading(false)
+        })
+      }, 1000)
+  }, [])// if empty, the array makes it only run on mounted
 
   return (
     <div className="home">
+      {loading && <p>Loading...</p>}
       {blogs &&<BlogList blogs={blogs} title="All Blogs"></BlogList>}
     </div>
   );
