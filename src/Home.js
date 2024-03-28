33d589f58bd44f7752b53dd62162fa3b35805d13
Luke <ljhorner2@gmail.com>
2024-03-28 15:06:51 +0000

    L19

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
-import BlogList from "./BlogList";
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList"
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [loading, setLoading] = useState(false)
-
-
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     // runs every time the data changes / renders
@@ -13,22 +12,30 @@ const Home = () => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          if (!res.ok){
+            throw Error('could not fetch data')
+          }
           return res.json()
         })
         .then((data) => {
-          console.log(data)
           setBlogs(data)
           setLoading(false)
+          setError(null)
+        })
+        .catch(e => {
+          setLoading(false)
+          setError(e.message)
         })
       }, 1000)
   }, [])// if empty, the array makes it only run on mounted
 
   return (
     <div className="home">
-      {loading && <p>Loading...</p>}
-      {blogs &&<BlogList blogs={blogs} title="All Blogs"></BlogList>}
+      {error && <p>{error}</p>}
+        {loading && <p>Loading...</p>}
+        {blogs &&<BlogList blogs={blogs} title="All Blogs"></BlogList>}
     </div>
-  );
+  )
 }
  
-export default Home;
+export default Home
