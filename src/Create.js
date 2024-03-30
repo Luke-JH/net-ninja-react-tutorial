82a909c26fe9c179e4fd35dc916bd0576322bf27
Luke <ljhorner2@gmail.com>
2024-03-30 16:25:43 +0000

    L30

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
     const [loading, setLoading] = useState(false)
+    const history = useHistory()
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -21,6 +23,8 @@ const Create = () => {
         .then(() => {
             console.log('new blog added')
             setLoading(false)
+            // history.go(-1)
+            history.push('/')
         })
     }
 
