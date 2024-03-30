c8f25adf626a18dcc4c162deee50c0bed7fd7193
Luke <ljhorner2@gmail.com>
2024-03-30 16:21:42 +0000

    L29

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,23 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = {title, body, author}
-        console.log(blog)
+        
+        setLoading(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog),
+        })
+        .then(() => {
+            console.log('new blog added')
+            setLoading(false)
+        })
     }
 
     return (
@@ -36,7 +48,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!loading && <button>Add Blog</button>}
+                {loading && <button disabled>Adding blog...</button>}
             </form>
         </div>
     )
